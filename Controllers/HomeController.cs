f3ca2640f6d429270c445a756a28c91cac2815dd
Anisong Chanthalalay <basically.tata@gmail.com>
2019-07-08 17:33:16 -0500

    Dojo Survey

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using DojoSurvey.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DojoSurvey.Controllers {
+    public class HomeController : Controller {
+        [HttpGet ("")]
+        public IActionResult Method () 
+        {
+            return View ("Method");
+        }
+
+        
+        [HttpPost]
+        [Route ("result")]
+        public IActionResult Result (string name, string location, string language, string comment) 
+        {
+            ViewBag.Name = name;
+            ViewBag.Location = location;
+            ViewBag.Language = language;
+            ViewBag.Comment = comment;
+            return View ("Result");
+        }
+    
+        
+
+    }
+}
---
+++ Models/MyModel.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace DojoSurvey.Models
+{
+    public class MyModel
+    {
+       
+    }
+}
